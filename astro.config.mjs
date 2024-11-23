8d831b03866dce5b0f0b5c296fbbfd96c5573d4a
Pierre <Pierre.helin@gmail.com>
2024-11-23 21:47:45 +0100

    debugs page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://helinp.github.io',
-  base: '/portfolio/',
+  base: '/portfolio',
 });
